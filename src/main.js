a5787559e6e1311c4b27dfbaa9334400ceb5ace3
aydenote <dslgpgh@gmail.com>
2022-05-02 20:27:44 +0900

    자판기 콜라 이미지 json 으로 구현

--- src/main.js
+++ src/main.js
@@ -1,2 +1,37 @@
 'use strict';
 
+function getJson(){
+    return fetch('/cola.json')
+    .then(response=>response.json())
+    .then(json=>json.items)
+    .catch(error=> alert(error))
+}
+
+function displayItems(items){ 
+    const container = document.querySelector('.list-cola'); 
+    container.innerHTML = items.map(item=>createHTMLString(item)).join('');
+}
+
+function createHTMLString(item){
+    console.log(item);
+    console.log(item.type.slice(-5));
+    console.log(item.type)
+    return `
+    <li class="list-${item.color}">
+    <img
+      src=${item.image}
+      width="36px"
+      height="65px"
+      class="img-${item.color}"
+      alt="보라색 콜라"
+    />
+    <p class="txt-colaName">${item.type}</p>
+    <p class="txt-price">${item.price}원</p>
+  </li>
+  `
+}
+
+getJson()
+    .then(items=>{
+        displayItems(items)
+    });
