c70492dbb75a328537545e100705b5ca656abec1
aydenote <dslgpgh@gmail.com>
2022-05-02 19:44:00 +0900

    클릭된 콜라만 json에서 data 호출 기능 구현

--- src/main.js
+++ src/main.js
@@ -2,12 +2,11 @@
 
 // data.json 가져오기
 function loadItems(){    
-    return fetch('./data.json') 
+    return fetch('./cola.json') 
     .then(response=> response.json())
     .then(json => json.items)
     .catch(error => alert(error))
 }
-
 
 const list_cola = document.querySelector(".list-cola")
 list_cola.addEventListener('click', (event)=>{
@@ -15,18 +14,37 @@ list_cola.addEventListener('click', (event)=>{
     if(event.target.className==="list-cola"){
         return 
     } else{
-        console.log("1");
+        //  클릭시 json data 호출
+        const type = event.target.className.slice(5); 
+        loadItems().then(items => {
+            for(let i=0; i<items.length; i++){
+              if(type.toUpperCase()===items[i].type.toUpperCase())
+              display(items[i]);
+            }
+        })
     }
 })
 
-function a(items){
-    console.log(items);
+// 클릭된 아이템 보여주기
+function display(items){
+    const container = document.querySelector('.con-getCola'); 
+    container.innerHTML = createHTMLString(items);
+    // items.map(item=>createHTMLString(item)).join('');
 }
 
-
-// 함수 호출
-loadItems() 
-.then(items => {
-    a(items);
-    //setEventListeners(items);
-})
+// 클릭된 아이템 HTML 생성
+function createHTMLString(item){
+    let first_count = 1;
+    return `
+    <div class="con-cola">
+    <img
+      src=${item.image}
+      width="18px"
+      height="33px"
+      class="img-cola"
+    />
+    <p class="txt-colaName voucher">${item.type}_cola</p>
+    <p class="txt-colaCount">${first_count}</p>
+  </div>
+  `
+}
