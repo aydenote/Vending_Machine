e994ce08c2d43dffee4420c1913502024abee8c9
aydenote <dslgpgh@gmail.com>
2022-05-03 14:33:29 +0900

    컨벤션에 따라 CSS 구조 변경

--- src/main.js
+++ src/main.js
@@ -45,9 +45,13 @@ const txt_deposit = document.querySelector(".txt-deposit");
 const my_money = document.querySelector(".txt-money");
 
 btn_Deposit.addEventListener('click',()=>{
-  my_money.innerText = txt_deposit.value+'원';
+  console.log(txt_deposit.value);
+ console.dir(my_money);
+  my_money.innerText = parseInt(txt_deposit.value) + parseInt(my_money.value)+'원';  // txt_deposit.value+'원';
+ 
   // 소지금 공백에 대한 예외처리
   if(txt_deposit.value==''){
+    console.log(my_money.value);
     my_money.innerText = 1000+'원';
   } 
   txt_deposit.value=null;
